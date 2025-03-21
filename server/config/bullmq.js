b7cc674484cefafa0a44d094569c10d25d323eee
novneetsingh <novneet300@gmail.com>
2025-03-21 23:44:42 +0530

    some fixes

--- server/config/bullmq.js
+++ server/config/bullmq.js
@@ -3,7 +3,7 @@ const Redis = require("ioredis");
 
 const redisConfig = new Redis(
   {
-    husername: process.env.REDIS_USERNAME,
+    username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
